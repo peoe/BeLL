161452c866e84e0dc7aa1c7c1f5d9f3cc6e741b9
kayosdark <oehme.pb@gmail.com>
2017-02-06 09:53:05 +0100

    fixed shit up

---
+++ src/rend/Pole.java
@@ -0,0 +1,55 @@
+package rend;
+
+import java.util.ArrayList;
+
+import graph.*;
+
+public class Pole {
+	
+	private Point p;
+	private ArrayList<Point> Corners;
+	private double height;
+	
+	public Pole(Point P, ArrayList<Point> C,double H){
+		p=P;
+		Corners=C;
+		height=H;
+	}
+
+	public Point getP() {
+		return p;
+	}
+
+	public void setP(Point p) {
+		this.p = p;
+	}
+
+	public ArrayList<Point> getCorners() {
+		return Corners;
+	}
+
+	public void setCorners(ArrayList<Point> corners) {
+		Corners = corners;
+	}
+	
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public String renderPole(){
+		
+		final int radius = 3;
+		
+		String finalstring = "";
+		finalstring=finalstring.concat("//Generated pole point " + getP());
+		finalstring=finalstring.concat(String.format(addcyl, getHeight(),(double)radius));
+		
+		
+		return finalstring;
+	}
+
+}
