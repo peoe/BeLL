5f1c6ff513ea53916e0b58723e7f9e33e4190f96
kayosdark <oehme.pb@gmail.com>
2017-12-09 18:19:11 +0100

    Updates formatting of Node

--- src/graph/Node.java
+++ src/graph/Node.java
@@ -3,7 +3,7 @@ package graph;
 import java.util.ArrayList;
 
 public class Node {
-	
+
 	// origin (coordinates) of Node
 	private Vector origin;
 	// incident Edge
@@ -11,13 +11,15 @@ public class Node {
 
 	/**
 	 * Constructor of the Node class.
-	 * @param v the Vector of the Nodes point
+	 * 
+	 * @param v
+	 *            the Vector of the Nodes point
 	 */
 	public Node(Vector v) {
 		origin = v;
 		incidentEdge = null;
 	}
-	
+
 	/**
 	 * Checks if two Nodes are equal.
 	 * 
@@ -26,17 +28,17 @@ public class Node {
 	 * @return true if both Nodes are equal
 	 */
 	public Boolean equals(Node n) {
-		if (n.getOrigin().getX() == origin.getX() && n.getOrigin().getY() == origin.getY()) 
-		{
+		if (n.getOrigin().getX() == origin.getX() && n.getOrigin().getY() == origin.getY()) {
 			return true;
 		}
-		
+
 		return false;
 	}
 
 	// getters - setters
 	/**
 	 * Returns the origin of the Node.
+	 * 
 	 * @return Vector pointing to the origin
 	 */
 	public Vector getOrigin() {
@@ -45,7 +47,9 @@ public class Node {
 
 	/**
 	 * Sets the origin of the Node.
-	 * @param origin the Vector pointing to the origin
+	 * 
+	 * @param origin
+	 *            the Vector pointing to the origin
 	 */
 	public void setOrigin(Vector origin) {
 		this.origin = origin;
@@ -53,6 +57,7 @@ public class Node {
 
 	/**
 	 * Returns the incident Edge of the Node.
+	 * 
 	 * @return incident Edge
 	 */
 	public Edge getIncidentEdge() {
@@ -61,32 +66,36 @@ public class Node {
 
 	/**
 	 * Sets the incident Edge of the Node.
-	 * @param incidentEdge the Edge to be set as incident Edge
+	 * 
+	 * @param incidentEdge
+	 *            the Edge to be set as incident Edge
 	 */
 	public void setIncidentEdge(Edge incidentEdge) {
 		this.incidentEdge = incidentEdge;
 	}
-	
+
 	/**
 	 * Returns all adjacent Edges pointing away from the Node.
+	 * 
 	 * @return ArrayList of adjacent Edges
 	 */
-	public ArrayList<Edge> getAdjacentEdges(){
+	public ArrayList<Edge> getAdjacentEdges() {
 		ArrayList<Edge> adjacentEdges = new ArrayList<>();
-		
+
 		Edge edge = getIncidentEdge();
-		
+
 		// add all adjacent edges
-		do{
+		do {
 			adjacentEdges.add(edge);
 			edge = edge.getTwin().getNext();
-		} while( edge != getIncidentEdge());
-		
+		} while (edge != getIncidentEdge());
+
 		return adjacentEdges;
 	}
-	
+
 	/**
 	 * Returns a String of the Node.
+	 * 
 	 * @return String of the Node
 	 */
 	public String toString() {
