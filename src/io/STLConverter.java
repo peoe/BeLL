da673462cccc42af1d8c057356f8ee7713b3bdd4
kayosdark <oehme.pb@gmail.com>
2017-12-10 11:42:24 +0100

    Updates comments and formatting of STLConverter

--- src/io/STLConverter.java
+++ src/io/STLConverter.java
@@ -5,40 +5,45 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class STLConverter {
-	//private static String openSCADLocation = System.getenv("ProgramFiles") + "/OpenSCAD/openscad.exe";
+	// the location of openscad.exe
 	private static String openSCADLocation = "";
 
 	/**
-	 * Converts a certain scad file into stl.
-	 * The file names will be taken over.
+	 * Converts a .scad File within the result folder into a .stl File with the
+	 * same filename.
 	 * 
-	 * @param fileName name of the file
-	 * @param fodlerName the name of the output folder
+	 * @param fileName
+	 *            the name of the File
+	 * @param fodlerName
+	 *            the name of the result folder
 	 */
-	public static void convert (String fileName, String folderName) throws InterruptedException {
+	public static void convert(String fileName, String folderName) throws InterruptedException {
 		Process p;
 		ProcessBuilder b;
 		try {
-			b = new ProcessBuilder(openSCADLocation + "\\openscad.exe", "-o", folderName + "\\stl\\" + fileName + ".stl", folderName + "\\scad\\" + fileName + ".scad");
-			//b.environment().put("PATH", openSCADLocation);
-			
-			//p = Runtime.getRuntime().exec("openscad -o " + folderName + "\\stl\\" + fileName + ".stl " + folderName + "\\scad\\" + fileName + ".scad");
+			// using processbuilder to avoid having to use path variables
+			b = new ProcessBuilder(openSCADLocation + "\\openscad.exe", "-o",
+					folderName + "\\stl\\" + fileName + ".stl", folderName + "\\scad\\" + fileName + ".scad");
+
+			// start the process
 			p = b.start();
 
+			// catch possible read/write errors
 			new Thread(new Runnable() {
-			    public void run() {
-			     BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			     String line = null; 
+				public void run() {
+					BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+					String line = null;
 
-			     try {
-			        while ((line = input.readLine()) != null)
-			            System.out.println(line);
-			     } catch (IOException e) {
-			            e.printStackTrace();
-			     }
-			    }
+					try {
+						while ((line = input.readLine()) != null)
+							System.out.println(line);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
 			}).start();
 
+			// wait until process is finished
 			p.waitFor();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -47,10 +52,12 @@ public class STLConverter {
 
 	/**
 	 * Sets the path to the OpenSCAD executable used by the STLConverter.
-	 * @param openSCADLocation the path to the executable
+	 * 
+	 * @param openSCADLocation
+	 *            the path to the OpenSCAD executable
 	 */
 	public static void setOpenSCADLocation(String openSCADLocation) {
 		STLConverter.openSCADLocation = openSCADLocation;
 	}
-	
+
 }
