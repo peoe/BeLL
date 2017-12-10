2c70c2d6b6e38fd692b03ab8dbbe1df92e19ab58
kayosdark <oehme.pb@gmail.com>
2017-12-10 11:36:13 +0100

    Updates comments and formatting of SCADFinder

--- src/io/SCADFinder.java
+++ src/io/SCADFinder.java
@@ -4,19 +4,24 @@ import java.io.File;
 import java.io.FilenameFilter;
 
 public class SCADFinder {
-	
-	/**
-	 * Returns an array of files in the given folder.
-	 * @param folderName the name of the specified folder
-	 * @return array of files in the specified folder
-	 */
-	public static File[] findFiles(String folderName){
-        File dir = new File(".\\" + folderName + "\\scad\\");
 
-        return dir.listFiles(new FilenameFilter() { 
-                 public boolean accept(File dir, String filename)
-                      { return filename.endsWith(".scad"); }
-        });
-    }
-	
+	/**
+	 * Returns an array of .scad Files in the result folder.
+	 * 
+	 * @param folderName
+	 *            the name of the result folder
+	 * @return array of .scad Files
+	 */
+	public static File[] findFiles(String folderName) {
+		// get link to the scad folder
+		File dir = new File(".\\" + folderName + "\\scad\\");
+
+		// return all .scad files
+		return dir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String filename) {
+				return filename.endsWith(".scad");
+			}
+		});
+	}
+
 }
