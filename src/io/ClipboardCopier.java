f4e0bbbd612644c925e9201e188acb59b235eb22
kayosdark <oehme.pb@gmail.com>
2017-12-10 11:16:35 +0100

    Updates comments and formatting of ClipboardCopier

--- src/io/ClipboardCopier.java
+++ src/io/ClipboardCopier.java
@@ -1,10 +1,17 @@
 package io;
+
 import java.awt.datatransfer.*;
 import java.awt.Toolkit;
 
 public class ClipboardCopier {
-	
-	public static void copyToClipboard(String s){
+
+	/**
+	 * Copies the String to your clipboard.
+	 * 
+	 * @param s
+	 *            the String to be copied
+	 */
+	public static void copyToClipboard(String s) {
 		StringSelection stringSelection = new StringSelection(s);
 		Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
 		clpbrd.setContents(stringSelection, null);
