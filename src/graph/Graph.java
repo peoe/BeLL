4ef2f3f745e6bc02bb270346152d796b3dc69118
SkaranYT <JoBa2902@googlemail.com>
2017-12-11 21:58:52 +0100

    3.2 Rewrite, Bubble Sort change

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -46,15 +46,15 @@ public class Graph {
 	private void processData(ArrayList<Line> ls) {
 		int i = 0;
 
-		do {
-			if (ls.get(i).toVector().getLength() > maxPrintWidth) {
-				ls.add(new Line(ls.get(i).getP1(), ls.get(i).getP1().add(ls.get(i).toVector().multiply(0.5))));
-				ls.add(new Line(ls.get(i).getP2(), ls.get(i).getP2().add(ls.get(i).toVector().multiply(-0.5))));
-				ls.remove(ls.get(i));
-			} else {
-				i += 1;
-			}
-		} while (i != ls.size() - 1);
+//		do {
+//			if (ls.get(i).toVector().getLength() > maxPrintWidth) {
+//				ls.add(new Line(ls.get(i).getP1(), ls.get(i).getP1().add(ls.get(i).toVector().multiply(0.5))));
+//				ls.add(new Line(ls.get(i).getP2(), ls.get(i).getP2().add(ls.get(i).toVector().multiply(-0.5))));
+//				ls.remove(ls.get(i));
+//			} else {
+//				i += 1;
+//			}
+//		} while (i != ls.size() - 1);
 
 		// create nodes and egdes
 		for (Line l : ls) {
@@ -173,27 +173,32 @@ public class Graph {
 	 *            of ArrayList containing all adjacent Edges
 	 */
 	private void sortEdges(ArrayList<ArrayList<Edge>> e) {
-		ArrayList<Double> doubles = new ArrayList<>();
-
+		ArrayList<Edge> sortedEdges = new ArrayList<>();
+		
 		for (ArrayList<Edge> eList : e) {
-			doubles.clear();
+			sortedEdges.clear();
 			for (Edge edg : eList) {
-				doubles.add(edg.toVector().angle());
-				double temp = 0.0;
-				Edge tempEdg = null;
-				for (int i = 0; i < doubles.size() - 1; i++) {
-					for (int j = i + 1; j < doubles.size(); j++) {
-						if (doubles.get(i) > doubles.get(j)) {
-							temp = doubles.get(i);
-							doubles.set(i, doubles.get(j));
-							doubles.set(j, temp);
-							tempEdg = eList.get(i);
-							eList.set(i, eList.get(j));
-							eList.set(j, tempEdg);
-						}
+				int index = 0;
+				for(int i = 0; i < sortedEdges.size(); i++){
+					if(sortedEdges.get(i).toVector().angle() > edg.toVector().angle()){
+						index = i;
+						break;
+					}
+					if(i == sortedEdges.size() - 1){
+						index = i + 1;
 					}
 				}
+					
+				if (index != sortedEdges.size()){
+					sortedEdges.add(index, edg);
+				} else {
+					sortedEdges.add(edg);
+				}
+					
+				
 			}
+			eList = (ArrayList<Edge>) sortedEdges.clone();
+			
 		}
 	}
 
