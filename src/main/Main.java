2aafa6ce84acba255b2a187cbb31a1c1747f33a1
SkaranYT <JoBa2902@googlemail.com>
2017-12-15 17:21:38 +0100

    reverted Main changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -51,9 +51,7 @@ public class Main {
 		try {
 			// creating scadprocessor using targetfile and params object
 			ScadProcessor proc = new ScadProcessor(DXFReader.getAutocadFile(targetFile), p);
-			
-			ClipboardCopier.copyToClipboard(new CornerPin(proc.getCorners().get(0).getN().getAdjacentEdges().get(0), 0, p).toString());
-			System.out.println(new CornerPin(proc.getCorners().get(0).getN().getAdjacentEdges().get(1), 0, p).toString());
+	
 			// for showing the result folder
 			getGui().getShowResultButton().setResultFolder(folderName);
 
